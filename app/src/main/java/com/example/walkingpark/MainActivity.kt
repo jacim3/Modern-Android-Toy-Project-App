ac2b4102ed3b3e1a9715d25e98c2143cf0e7daf2
Jack Kim <60813834+jacim3@users.noreply.github.com>
2022-04-11 23:53:16 +0900

    'dfaf'

--- app/src/main/java/com/example/walkingpark/MainActivity.kt
+++ app/src/main/java/com/example/walkingpark/MainActivity.kt
@@ -37,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Log.e("mainActivity", viewModel.hashCode().toString())
         locationServiceRepository.locationCallback = viewModel.locationCallback
